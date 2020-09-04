7a26ea632397f6935b3bb0c4f8113ca2d387a0ef
IshaqMccann <70007826+IshaqMccann@users.noreply.github.com>
2020-09-04 21:56:43 +0300

    Update post.tsx

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -121,6 +121,9 @@ const PageTemplate = ({ data, pageContext, location }: PageTemplateProps) => {
       <Helmet>
         <html lang={config.lang} />
         <title>{post.frontmatter.title}</title>
+        <script>
+        {`console.log("ads")`}
+        </script>
 
         <meta name="description" content={post.frontmatter.excerpt || post.excerpt} />
         <meta property="og:site_name" content={config.title} />
