344f0d17b0d029d80a96386dfacea6c509910580
IshaqMccann <70007826+IshaqMccann@users.noreply.github.com>
2020-09-22 20:50:37 +0300

    sa
    asd

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: 'Донбасс юридический',
@@ -23,13 +27,13 @@ module.exports = {
       resolve: 'gatsby-plugin-firebase',
       options: {
         credentials: {
-          apiKey: 'AIzaSyBf7OaR5bJCs44H_pMPR4Bb0AYCEGPgHf8',
-          authDomain: '<YOUR_FIREBASE_AUTH_DOMAIN>',
-          databaseURL: '<YOUR_FIREBASE_DATABASE_URL>',
-          projectId: '<YOUR_FIREBASE_PROJECT_ID>',
-          storageBucket: '<YOUR_FIREBASE_STORAGE_BUCKET>',
-          messagingSenderId: '<YOUR_FIREBASE_MESSAGING_SENDER_ID>',
-          appId: '<YOUR_FIREBASE_APP_ID>',
+          apiKey: process.env.API_KEY,
+          authDomain: process.env.AUTH_DOMAIN,
+          databaseURL: process.env.DATABASE_URL,
+          projectId: process.env.PROJECT_ID,
+          storageBucket: process.env.STORAGE_BUCKET,
+          messagingSenderId: process.env.MESSAGING_SENDER_ID,
+          appId: process.env.APP_ID,
         },
       },
     },
